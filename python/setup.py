4a8a0e9eb77c0eaddccd7ed975f41c094181028a
tastatur <repkov.denis@gmail.com>
2015-01-11 15:44:33 +0100

    Python setup package created

---
+++ python/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cberry',
+      description='Python wrapper for the admatec cberry library',
+      packages=['cberry'],
+     )
